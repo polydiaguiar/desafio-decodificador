12a564a19fc6bce65dd7a71949f0cc7fd452291e
Poly (not the doll) <152785008+polydiaguiar@users.noreply.github.com>
2024-03-18 20:39:50 -0300

    Incluir responsividade.

---
+++ script.js
@@ -0,0 +1,14 @@
+var botaoCriptografar = document.querySelector(".botao_criptografar");
+var botaoDescriptografar = document.querySelector(".botao_descriptografar");
+var area_output_pessoa = document.querySelector(".area_output_pessoa");
+var area_output = document.querySelector(".area_output_pessoa");
+var resultado = document.querySelector(".area_output_texto_resultado");
+
+botaoCriptografar.onclick= criptografar;
+botaoDescriptografar.onclick= descriptografar;
+
+function criptografar(){
+    ocultar
+
+
+}
